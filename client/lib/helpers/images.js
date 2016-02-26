475d43bbb97e330a69ab50d582966799fadd9414
Mario Blas <marioblasgc@gmail.com>
2016-02-26 20:03:21 +0000

    Update dev dependencies

--- client/lib/helpers/images.js
+++ client/lib/helpers/images.js
@@ -21,12 +21,11 @@ convertImgToBase64URL = (url, callback, outputFormat) => {
   img.onload = () => {
     let canvas = document.createElement('CANVAS');
     const ctx = canvas.getContext('2d');
-    let dataURL;
 
     canvas.height = img.height;
     canvas.width = img.width;
     ctx.drawImage(img, 0, 0);
-    dataURL = canvas.toDataURL(outputFormat);
+    const dataURL = canvas.toDataURL(outputFormat);
     callback(dataURL);
     canvas = null;
   };
